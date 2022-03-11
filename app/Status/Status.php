88cea4d77a175151d4bf00061ad61c617647f66e
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-11 21:51:08 +0100

    Fill avalabel status

--- app/Status/Status.php
+++ app/Status/Status.php
@@ -8,5 +8,19 @@ class Status
     public const NON_TRAITE = 1;
     public const RETOUR = 2;
     public const LIVRE = 3;
-
+    public const CHANGE = 4;
+    public const ENCOURS = 5;
+    public const EXPEDIE = 6;
+    public const INJOIGNABLE = 7;
+    public const INTERESSE = 8;
+    public const MANQUE_DE_STOCK = 9;
+    public const PAS_DE_REPONSE = 10;
+    public const PAS_DE_REPONSE_2 = 11;
+    public const PAS_DE_REPONSE_3 = 12;
+    public const PAS_DE_REPONSE_4 = 13;
+    public const PAS_DE_REPONSE_5 = 14;
+    public const RECONFIRMER = 15;
+    public const REFUSE = 16;
+    public const REPORTE = 17;
+    public const REROURNE = 18;
 }
