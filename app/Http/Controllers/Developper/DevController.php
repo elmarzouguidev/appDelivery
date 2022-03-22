1b4f7388bf5eafecba4898cfe684e84e4294fe93
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-22 09:13:37 +0100

    livewire proble m

--- app/Http/Controllers/Developper/DevController.php
+++ app/Http/Controllers/Developper/DevController.php
@@ -83,11 +83,13 @@ class DevController extends Controller
 
     public function livewireConfig()
     {
-        Artisan::call('livewire:publish', ['--config']);
+        //Artisan::call('livewire:publish', ['--config' => true]);
+        Artisan::call('vendor:publish', ['--force' => true, '--tag' => 'livewire:config']); 
     }
 
     public function livewireAssets()
     {
-        Artisan::call('livewire:publish', ['--assets']);
+        //Artisan::call('livewire:publish', ['--assets' => true]);
+        Artisan::call('vendor:publish', ['--force' => true, '--tag' => 'livewire:assets']); 
     }
 }
