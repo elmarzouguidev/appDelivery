7048502cb075c4e8639314ffafccd880cd8b50d3
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-04-02 21:14:34 +0200

    Puusher

--- resources/views/layouts/_parts/__sameleon_admin.blade.php
+++ resources/views/layouts/_parts/__sameleon_admin.blade.php
@@ -79,22 +79,22 @@
     </li>
 @endif
 
-<li>
+@if (auth()->user()->hasAnyRole('Admin', 'SuperAdmin'))
+    <li>
 
-    <a href="javascript: void(0);" class="has-arrow waves-effect">
-        <i class="bx bx-wrench"></i>
-        <span key="t-settings">{{ __('Settings') }}</span>
-    </a>
-     <ul class="sub-menu" aria-expanded="false">
-        <li>
-            <a href="{{ route('admin:settings.index') }}"
-                key="t-company">{{ __('Société') }}
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('admin:settings.invoice') }}"
-                key="t-invoice">{{ __('Facture') }}
-            </a>
-        </li>
-    </ul> 
-</li>
+        <a href="javascript: void(0);" class="has-arrow waves-effect">
+            <i class="bx bx-wrench"></i>
+            <span key="t-settings">{{ __('Settings') }}</span>
+        </a>
+        <ul class="sub-menu" aria-expanded="false">
+            <li>
+                <a href="{{ route('admin:settings.index') }}" key="t-company">{{ __('Société') }}
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin:settings.invoice') }}" key="t-invoice">{{ __('Facture') }}
+                </a>
+            </li>
+        </ul>
+    </li>
+@endif
