46f1e815053e22bdf97fc096832f43b0c14b9fff
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-04-18 15:38:49 +0200

    add validation to fix prblem in bigInteger v7

---
+++ resources/views/Sameleon/Admin/Contact/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <h1>Comming Song ...</h1>
+    </div>
+@endsection
+
+@once
+    @push('scripts')
+    @endpush
+@endonce
