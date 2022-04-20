67b9081eaccc9f0ba133157ccf7c6cef7b816b94
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-04-20 10:49:16 +0200

    fix invoice prb

--- resources/views/Sameleon/PDF/invoice.blade.php
+++ resources/views/Sameleon/PDF/invoice.blade.php
@@ -172,7 +172,7 @@
                 -R.C:{{ optional(getCompany())->rc }}
                 -PATENTE:{{ optional(getCompany())->patente }}
                 -I.F:{{ optional(getCompany())->if }}
-                @if(isset(getCompany())->cnss))
+                @if(isset(getCompany()->cnss))
                 -CNSS:{{ optional(getCompany())->cnss }}
                 @endif
                 -ICE:{{ optional(getCompany())->ice }}
