89c2ac2066645cbb9352d453a5fa342883b58ef1
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-09 00:04:26 +0100

    try to fix data priority in commands v2

--- app/Http/Livewire/Sameleon/Command/Commands.php
+++ app/Http/Livewire/Sameleon/Command/Commands.php
@@ -67,7 +67,7 @@ class Commands extends Component
                 ->with(['invoice:uuid,id,full_number', 'city:id,name'])
                 ->orderByRaw("created_at DESC")
                 ->get()->prioritize(function ($item) {
-                    return $item->status == Status::LIVRE
+                    return $item->status == Status::LIVRE;
                 });
         } else {
 
