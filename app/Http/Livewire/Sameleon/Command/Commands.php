a7cf017b0bb86251a60518b388dbfebf7fac16bf
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-09 06:46:02 +0100

    update Edit stock CRUD systeme v4

--- app/Http/Livewire/Sameleon/Command/Commands.php
+++ app/Http/Livewire/Sameleon/Command/Commands.php
@@ -175,7 +175,7 @@ class Commands extends Component
 
                 $qteRest = $qteGlobal - $product->pivot->quantity;
 
-                if ($qteRest < $qteGlobal && $product->stock->qte_livre != $product->stock->qte_global) {
+                if ($qteRest < $qteGlobal && $product->stock->qte_livre != $product->stock->qte_global && $product->pivot->quantity) {
                     $product->stock()->increment('qte_livre', $product->pivot->quantity);
                     $product->stock()->update(['qte_rest' => $product->stock->qte_rest - $product->pivot->quantity]);
                 }
@@ -189,7 +189,7 @@ class Commands extends Component
 
             $products->each(function ($product, $key) {
 
-                if($product->stock->qte_livre > 0)
+                if($product->stock->qte_livre > 0 && $product->pivot->quantity)
                 {
                     $product->stock()->decrement('qte_livre', $product->pivot->quantity);
                     $product->stock()->update(['qte_rest' => $product->stock->qte_rest + $product->pivot->quantity]);
