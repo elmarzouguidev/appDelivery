6f72e45c246d75752e0a147f65759f4d6b21774a
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-05-09 12:55:57 +0200

    fix same stuff in Invoice PDF file v7

--- resources/views/Sameleon/PDF/invoice.blade.php
+++ resources/views/Sameleon/PDF/invoice.blade.php
@@ -248,10 +248,10 @@
          
                 <tr class="item {{ $loop->last ? 'last' : '' }}">
                     <td style="width: 30% ;">{{ $article->code_command }}</td>
-                    @if($article->command->status != App\Status\Status::LIVRE)
-                    <td>{{ optional($article->command)->updated_at->format('d-m-Y') ?? '' }}</td>
+                    @if(optional($article->command)->status != App\Status\Status::LIVRE)
+                     <td>{{ optional($article->command)->updated_at->format('d-m-Y') ?? '' }}</td>
                     @else
-                    <td>{{ optional($article->command)->delivered_at->format('d-m-Y') ?? '' }}</td>
+                     <td>{{ optional($article->command)->delivered_at->format('d-m-Y') ?? '' }}</td>
                     @endif
                     <td>{{ $article->city }}</td>
                     <td>{{ $article->status }}</td>
