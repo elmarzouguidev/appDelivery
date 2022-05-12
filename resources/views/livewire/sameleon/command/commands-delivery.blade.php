2023e6b0dbad56eaa4a689d99d168be6bd63255c
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-05-12 15:52:47 +0100

    fix same stuff in Invoice system v9

--- resources/views/livewire/sameleon/command/commands-delivery.blade.php
+++ resources/views/livewire/sameleon/command/commands-delivery.blade.php
@@ -5,7 +5,7 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <div class=" mx-auto mb-4">
-                            <span class="avatar-title bg-primary bg-soft text-primary font-size-18">
+                            <span class="avatar-title bg-primary bg-soft text-primary font-size-20">
                             
                                 {{ number_format($command->products_sum_product_commandprice_total, 2) }}DH
                                 
@@ -23,26 +23,28 @@
                     
                         <hr>
                         <div>
-                            <a href="javascript: void(0);" class="badge bg-primary font-size-18 m-1">Livré</a>
+                            <a href="javascript: void(0);" class="badge bg-success font-size-18 m-1">Livré</a>
                             <a href="javascript: void(0);" class="badge bg-danger font-size-18 m-1">Non livre</a>
                             <a href="javascript: void(0);" class="badge bg-warning font-size-18 m-1">Non interese</a>
                             <a href="javascript: void(0);" class="badge bg-primary font-size-18 m-1">Pas de reponse</a>
                         </div>
                     </div>
                     <div class="card-footer bg-transparent border-top">
-                        <div class="contact-links d-flex font-size-14">
+                        <div class="contact-links d-flex font-size-17">
                             <div class="flex-fill">
                                 @foreach ($command->products as $product)
-
-                                <p class="text-strong mb-0">
-                                    <strong>{{ $product->name }}</strong>
-                                </p>
-                              
+                                
                                 <div>
-                                    <p class="text-muted mb-0">{{ $product->price }}(DH) x
+
+                                    <p class="text-strong mb-0">
+                                        <strong>{{ $product->name }}</strong>
+                                    </p>
+
+                                    <p class="text-muted mb-0">{{ $product->price }} (DH) x
                                         {{ $product->pivot->quantity }}
                                     </p>
                                     <br>
+
                                 </div>
                                 <hr>
                             @endforeach
