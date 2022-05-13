a4f4aeabdf1c87e5ae523898d5e0671e4dfecdfb
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-05-13 15:02:05 +0100

    make same stuuf in Seeders Classes v4

--- database/factories/Sameleon/StockFactory.php
+++ database/factories/Sameleon/StockFactory.php
@@ -15,11 +15,11 @@ class StockFactory extends Factory
     public function definition()
     {
         return [
-            'qte_global' => rand(1, 193),
+            'qte_global' => 100,
             'qte_livre' => 0,
             'qte_expidite' => 0,
             'qte_endomage' => 0,
-            'qte_rest' => 0,
+            'qte_rest' => 100,
 
             'active' => rand(1, 0),
             //'product_id' => rand(1, Product::count())
