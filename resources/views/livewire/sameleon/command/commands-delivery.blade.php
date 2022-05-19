03d230b27fab4fbe0316dff7d7a376fc01a02999
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-19 16:20:14 +0100

    fix same stuff in delivery Interface v8

--- resources/views/livewire/sameleon/command/commands-delivery.blade.php
+++ resources/views/livewire/sameleon/command/commands-delivery.blade.php
@@ -5,7 +5,7 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <div class=" mx-auto mb-4">
-                            <span class="avatar-title bg-primary bg-soft text-primary font-size-20">
+                            <span class="avatar-title bg-primary bg-soft text-primary font-size-25">
 
                                 {{ number_format($command->items_sum_prix_total, 2) }}DH
 
