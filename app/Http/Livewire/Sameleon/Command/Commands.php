80955f04f6cd14cfa498d193e2a0f60cc00b8439
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-19 16:09:38 +0100

    fix same stuff in delivery Interface v2

--- app/Http/Livewire/Sameleon/Command/Commands.php
+++ app/Http/Livewire/Sameleon/Command/Commands.php
@@ -121,7 +121,8 @@ class Commands extends Component
 
             $commands =  $command->where('delivery_id', auth()->id())
                 ->where('delivery_uuid', auth()->user()->uuid)
-                //->where('status', Status::ENCOURS)
+               // ->where('status', '!=', Status::LIVRE)
+                ->where('updated_at', now())
                 ->with('items')
                 ->withSum('items', 'prix_total')
                 //->with('products.stock')
