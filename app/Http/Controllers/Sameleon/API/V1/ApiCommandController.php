6a7e789c69f12f36bbd3849e4f773ab1fe26cbaa
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-23 02:57:06 +0100

    update same stuff in Command API

--- app/Http/Controllers/Sameleon/API/V1/ApiCommandController.php
+++ app/Http/Controllers/Sameleon/API/V1/ApiCommandController.php
@@ -70,7 +70,7 @@ class ApiCommandController extends Controller
                             return response()->json(
                                 [
 
-                                    '_response' => ['msg' => "désole le produit : {$item['name']} n'existe pas dans notre systeme"]
+                                    '_response' => ['msg' => "désole le produit ( {$item['name']} ) n'existe pas dans notre systeme"]
                                 ],
                                 404
                             );
