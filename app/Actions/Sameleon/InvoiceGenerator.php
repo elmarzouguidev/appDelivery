1d45d89706c119a275395694572549a14f986087
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-05-25 00:40:49 +0100

    Update InvoiceGenerator v4

--- app/Actions/Sameleon/InvoiceGenerator.php
+++ app/Actions/Sameleon/InvoiceGenerator.php
@@ -18,7 +18,7 @@ class InvoiceGenerator
 
     public function handle()
     {
-        $this->deleteNullInvoices();
+        //$this->deleteNullInvoices();
 
         $this->CloseYesterdayInvoice();
 
@@ -34,7 +34,7 @@ class InvoiceGenerator
                 $q->whereDay('delivered_at', now()->format('d'))
                     ->orWhereYear('delivered_at', '1993');
             })
-            ->doesntHave('articles')
+            //->doesntHave('articles')
             //->with('client:id,uuid')
             ->get();
 
