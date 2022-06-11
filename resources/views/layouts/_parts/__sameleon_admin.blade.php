99543e70ae4d7ebbf63872ef19606f78f7c2185a
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-06-12 00:51:23 +0100

    hide permissions Url from NavBar *

--- resources/views/layouts/_parts/__sameleon_admin.blade.php
+++ resources/views/layouts/_parts/__sameleon_admin.blade.php
@@ -1,58 +1,57 @@
-@if(auth()->user()->hasAnyRole('Client','SuperAdmin','Admin'))
-<li>
-    <a href="{{ route('admin:home') }}" class="waves-effect">
-        <i class="bx bx-home-circle"></i>{{-- <span class="badge rounded-pill bg-info float-end">04</span> --}}
-        <span key="t-dashboards">{{ __('navbar.dashboard') }}</span>
-    </a>
-</li>
+@if (auth()->user()->hasAnyRole('Client', 'SuperAdmin', 'Admin'))
+    <li>
+        <a href="{{ route('admin:home') }}" class="waves-effect">
+            <i class="bx bx-home-circle"></i>{{-- <span class="badge rounded-pill bg-info float-end">04</span> --}}
+            <span key="t-dashboards">{{ __('navbar.dashboard') }}</span>
+        </a>
+    </li>
 
-<li class="menu-title" key="t-stock">Stock</li>
-<li>
-    <a href="{{ route('admin:products.index') }}">
-        <i class="bx bx-store"></i>
-        <span key="t-products">{{ __('Produits') }}</span>
-    </a>
-</li>
-<li>
-    <a href="{{ route('admin:stock.index') }}" class="waves-effect">
+    <li class="menu-title" key="t-stock">Stock</li>
+    <li>
+        <a href="{{ route('admin:products.index') }}">
+            <i class="bx bx-store"></i>
+            <span key="t-products">{{ __('Produits') }}</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('admin:stock.index') }}" class="waves-effect">
 
-        <i class="bx bxs-box "></i>
-        @if($stock_out)
-            <span class="badge rounded-pill bg-danger float-end">{{$stock_out}}</span>
-        @endif
-        <span key="t-stock">{{ __('Stock') }}</span>
-    </a>
+            <i class="bx bxs-box "></i>
+            @if ($stock_out)
+                <span class="badge rounded-pill bg-danger float-end">{{ $stock_out }}</span>
+            @endif
+            <span key="t-stock">{{ __('Stock') }}</span>
+        </a>
 
-</li>
+    </li>
 @endif
 <li class="menu-title" key="t-commands">Commandes</li>
 
 <li>
     <a href="{{ route('admin:commands.index') }}" class="waves-effect">
 
-        
+
         <i class="bx bx-cart-alt"></i>
-        @if($total_new_command)
-         <span class="badge rounded-pill bg-info float-end">{{$total_new_command}}</span>
+        @if ($total_new_command)
+            <span class="badge rounded-pill bg-info float-end">{{ $total_new_command }}</span>
         @endif
         <span key="t-commands">{{ __('Commandes') }}</span>
     </a>
 
 </li>
 @if (auth()->user()->hasAnyRole('Admin', 'SuperAdmin'))
-
     <li class="menu-title" key="t-components">{{ __('Clients') }}</li>
 
     <li>
         <a href="{{ route('admin:clients.index') }}" class="waves-effect">
 
             <i class="bx bxs-user-detail"></i>
-            
+
             <span key="t-clients">{{ __('Clients') }}</span>
         </a>
 
     </li>
-    {{--<li>
+    {{-- <li>
         <a href="{{ route('admin:groups.index') }}" class="waves-effect">
 
             <i class="bx bx-group"></i>
@@ -60,62 +59,62 @@
             <span key="t-groups">{{ __('Groups') }}</span>
         </a>
 
-    </li>--}}
-@endif    
+    </li> --}}
+@endif
 
-@if(auth()->user()->hasAnyRole('Client','SuperAdmin','Admin'))
-<li class="menu-title" key="t-invoices">Factures</li>
+@if (auth()->user()->hasAnyRole('Client', 'SuperAdmin', 'Admin'))
+    <li class="menu-title" key="t-invoices">Factures</li>
 
-<li>
-    <a href="{{ route('admin:invoices.index') }}">
-        <i class="bx bx-file"></i>
-        @if($invoice_non_closed)
-          <span class="badge rounded-pill bg-info float-end">{{$invoice_non_closed}}</span>
-        @endif
-        <span key="t-invoices">{{ __('Factures') }}</span>
-    </a>
-</li>
-<li>
-    <a href="{{ route('admin:payments.index') }}">
-        <i class="bx bx-money"></i>
-        <span key="t-payments">{{ __('Paiements') }}</span>
-    </a>
-</li>
+    <li>
+        <a href="{{ route('admin:invoices.index') }}">
+            <i class="bx bx-file"></i>
+            @if ($invoice_non_closed)
+                <span class="badge rounded-pill bg-info float-end">{{ $invoice_non_closed }}</span>
+            @endif
+            <span key="t-invoices">{{ __('Factures') }}</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('admin:payments.index') }}">
+            <i class="bx bx-money"></i>
+            <span key="t-payments">{{ __('Paiements') }}</span>
+        </a>
+    </li>
 
-<li class="menu-title" key="t-invoices">Réclamations</li>
+    <li class="menu-title" key="t-invoices">Réclamations</li>
 
-<li>
-    <a href="{{ route('admin:complaints.index') }}">
-        <i class='bx bx-info-circle'></i>
-        @if($total_new_reclamations)
-         <span class="badge rounded-pill bg-danger float-end">{{$total_new_reclamations}}</span>
-        @endif
-        <span key="t-complaints">{{ __('Réclamations') }}</span>
-    </a>
-</li>
+    <li>
+        <a href="{{ route('admin:complaints.index') }}">
+            <i class='bx bx-info-circle'></i>
+            @if ($total_new_reclamations)
+                <span class="badge rounded-pill bg-danger float-end">{{ $total_new_reclamations }}</span>
+            @endif
+            <span key="t-complaints">{{ __('Réclamations') }}</span>
+        </a>
+    </li>
 
-<li class="menu-title" key="t-ramassage">Ramassage</li>
+    <li class="menu-title" key="t-ramassage">Ramassage</li>
 
-<li>
-    <a href="{{ route('admin:ramassage.index') }}">
-        <i class='bx bx-archive-in'></i>
-        @if($ramassage)
-         <span class="badge rounded-pill bg-danger float-end">{{$ramassage}}</span>
-        @endif
-        <span key="t-ramassage">{{ __('Ramassage') }}</span>
-    </a>
-</li>
+    <li>
+        <a href="{{ route('admin:ramassage.index') }}">
+            <i class='bx bx-archive-in'></i>
+            @if ($ramassage)
+                <span class="badge rounded-pill bg-danger float-end">{{ $ramassage }}</span>
+            @endif
+            <span key="t-ramassage">{{ __('Ramassage') }}</span>
+        </a>
+    </li>
 
-<li class="menu-title" key="t-invoices">Contact</li>
+    <li class="menu-title" key="t-invoices">Contact</li>
 
-<li>
-    <a href="{{ route('admin:contact.index') }}">
-        <i class='bx bx-envelope'></i>
-        <span key="t-contact">{{ __('Contact') }}</span>
-    </a>
-</li>
+    <li>
+        <a href="{{ route('admin:contact.index') }}">
+            <i class='bx bx-envelope'></i>
+            <span key="t-contact">{{ __('Contact') }}</span>
+        </a>
+    </li>
 
-@endif  
+@endif
 
 @if (auth()->user()->hasAnyRole('Admin', 'SuperAdmin'))
     <li class="menu-title" key="t-components">{{ __('navbar.advanced') }}</li>
@@ -143,8 +142,7 @@
         </a>
         <ul class="sub-menu" aria-expanded="false">
             <li><a href="{{ route('admin:cities.index') }}" key="t-cities">{{ __('Villes') }}</a></li>
-            <li><a href="{{ route('admin:regions.index') }}"
-                    key="t-regions">{{ __('Régions') }}</a>
+            <li><a href="{{ route('admin:regions.index') }}" key="t-regions">{{ __('Régions') }}</a>
             </li>
         </ul>
     </li>
@@ -166,7 +164,7 @@
         </a>
 
     </li>
-    <li>
+    {{-- <li>
         <a href="javascript: void(0);" class="has-arrow waves-effect">
             <i class="bx bx-lock"></i>
             <span key="t-authentication">{{ __('Permissions') }}</span>
@@ -177,7 +175,7 @@
                     key="t-permissions">{{ __('Permissions') }}</a>
             </li>
         </ul>
-    </li>
+    </li> --}}
 @endif
 
 @if (auth()->user()->hasAnyRole('Admin', 'SuperAdmin'))
