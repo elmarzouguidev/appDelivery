e2b92a1d163db9e1d3eec7d82eb42247787b6777
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-06-14 01:55:01 +0100

    add new assets files

---
+++ resources/views/layouts/_section/head_mix.blade.php
@@ -0,0 +1,19 @@
+<head>
+
+    <meta charset="utf-8" />
+    <title>SAMELEON EXPRESS System</title>
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="app_version" name="v 1.1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
+
+    @yield('css')
+    <link href="{{ asset('assets/libs/magnific-popup/magnific-popup.css') }}" rel="stylesheet" type="text/css" />
+
+    <link href="{{ asset('css/mix/app.css') }}?ver={{ rand(1, 852) }}" rel="stylesheet" type="text/css" />
+
+    @livewireStyles
+
+</head>
---
+++ resources/views/layouts/_section/head_normal.blade.php
@@ -0,0 +1,25 @@
+<head>
+
+    <meta charset="utf-8" />
+    <title>SAMELEON EXPRESS System</title>
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="app_version" name="v 1.1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
+
+    
+    <link href="{{ asset('assets/libs/magnific-popup/magnific-popup.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('css/normal/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('css/normal/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('css/normal/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+
+    @yield('css')
+    
+    @livewireStyles
+</head>
