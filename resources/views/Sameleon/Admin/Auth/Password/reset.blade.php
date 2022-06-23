6fa4928183188bedc53a7107ec578fb5fd15a7a6
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-06-23 18:43:00 +0100

    update stuff in rset password css url

--- resources/views/Sameleon/Admin/Auth/Password/reset.blade.php
+++ resources/views/Sameleon/Admin/Auth/Password/reset.blade.php
@@ -12,7 +12,7 @@
     <meta content="app_version" name="v 1.1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/mix/app.css') }}" rel="stylesheet" type="text/css" />
 
 </head>
 
