293fc444b4ce65ffb341652c0770e9dafa89f92f
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-10-05 01:02:59 +0100

    add Commands Livewire Componenent for Delivery Cuard

---
+++ app/Http/Livewire/Sameleon/Command/SubDelivery/Commands.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Sameleon\Command\SubDelivery;
+
+use Livewire\Component;
+
+class Commands extends Component
+{
+    public function render()
+    {
+        return view('livewire.sameleon.command.sub-delivery.commands');
+    }
+}
