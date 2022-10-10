fb8953493ee2f60199b9877d2558299ae753f074
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-10-10 10:11:28 +0100

    update Print CMD ticket Template

--- resources/views/Sameleon/PDF/print-command.blade.php
+++ resources/views/Sameleon/PDF/print-command.blade.php
@@ -6,248 +6,219 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>{{ $command->code }} - {{ $command->created_at->format('d-m-Y') }}</title>
     <style>
-        @page {
-            margin: 5px 10px;
-        }
-
         body {
-            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-            text-align: center;
-            color: #777;
+            font-size: 18px;
         }
 
-        body h1 {
-            font-weight: 300;
-            margin-bottom: 0px;
-            padding-bottom: 0px;
-            color: #000;
-        }
-
-        body h3 {
-            font-weight: 300;
-            margin-top: 5px;
-            margin-bottom: 5px;
-            font-style: italic;
-            color: #555;
-        }
-
-        body a {
-            color: #06f;
-        }
-
-        .invoice-box {
-            max-width: 1000px;
-            margin: auto;
-            padding: 1px;
-            border: 1px solid #eee;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-            font-size: 15px;
-            line-height: 24px;
-            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-            color: #555;
-        }
-
-        .invoice-box table {
+        table {
             width: 100%;
-            line-height: inherit;
-            text-align: left;
             border-collapse: collapse;
         }
 
-        .invoice-box table td {
-            padding: 5px;
-            vertical-align: top;
+        table tr td {
+            padding: 0;
         }
 
-        .invoice-box table tr td:nth-child(2) {
+        table tr td:last-child {
             text-align: left;
         }
 
-        .invoice-box table tr td:nth-child(3) {
-            text-align: left;
-        }
-
-        .invoice-box table tr td:nth-child(4) {
-            text-align: left;
-        }
-
-        .invoice-box table tr.top table td {
-            padding-bottom: 10px;
-        }
-
-        .invoice-box table tr.top table td.title {
-            font-size: 45px;
-            line-height: 45px;
-            color: #333;
-        }
-
-        .invoice-box table tr.information table td {
-            padding-bottom: 40px;
-        }
-
-        .invoice-box table tr.heading td {
-            background: #eee;
-            border-bottom: 2px solid #ddd;
+        .bold {
             font-weight: bold;
         }
 
-        .invoice-box table tr.heading-price td {
-            background: #eee;
-            /*border-bottom: 2px solid #325288;*/
-            font-weight: bold;
+        .right {
             text-align: right;
         }
 
-        .invoice-box table tr.details td {
-            padding-bottom: 10px;
+        .large {
+            font-size: 18px;
         }
 
-        .invoice-box table tr.item td {
-            border-bottom: 1px solid #eee;
-        }
-
-        .invoice-box table tr.item.last td {
-            border-bottom: none;
-        }
-
-        .invoice-box table tr.total td:nth-child(2) {
-            border-top: 2px solid #eee;
+        .total {
             font-weight: bold;
+            color: #1a0c0d;
         }
 
-        .invoice-box table tr.total td:nth-child(3) {
-            border-top: 2px solid #eee;
-            font-weight: bold;
+        .logo-container {
+            margin: 10px 0 10px 0;
         }
 
-        .bott {
-            height: 0px;
-            width: 200px;
-            border-bottom: solid #1572A1 20px;
+        .invoice-info-container {
+            font-size: 16px;
         }
 
-        @media only screen and (max-width: 600px) {
-            .invoice-box table tr.top table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-            }
-
-            .invoice-box table tr.information table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-            }
+        .invoice-info-container td {
+            padding: 4px 0;
         }
 
-        footer {
-            position: fixed;
-            bottom: -10px;
-            left: 0px;
-            right: 0px;
-            height: 50px;
-            /** Extra personal styles **/
+        .client-name {
+            font-size: 16px;
+            vertical-align: top;
+        }
 
-            color: white;
-            text-align: center;
-            line-height: 10px;
+        .line-items-container {
+            margin: 70px 0;
+            font-size: 16px;
+        }
+
+        .line-items-container th {
+            text-align: left;
+            color: rgb(19, 14, 14);
+            border-bottom: 2px solid #ddd;
+            padding: 10px 0 15px 0;
+            font-size: 16px;
+            text-transform: uppercase;
+        }
+
+        .line-items-container th:last-child {
+            text-align: left;
+        }
+
+        .line-items-container td {
+            padding: 15px 0;
+        }
+
+        .line-items-container tbody tr:first-child td {
+            padding-top: 25px;
+        }
+
+        .line-items-container.has-bottom-border tbody tr:last-child td {
+            padding-bottom: 25px;
+            border-bottom: 2px solid #ddd;
+        }
+
+        .line-items-container.has-bottom-border {
+            margin-bottom: 0;
+        }
+
+        .line-items-container th.heading-quantity {
+            width: 50px;
+        }
+
+        .line-items-container th.heading-price {
+            text-align: right;
+            width: 100px;
+        }
+
+        .line-items-container th.heading-subtotal {
+            width: 100px;
+        }
+
+        .payment-info {
+            width: 38%;
+            font-size: 16px;
+            line-height: 1.5;
+        }
+
+        .footer {
+            margin-top: 100px;
+        }
+
+        .footer-thanks {
+            font-size: 1.125em;
+        }
+
+        .footer-thanks img {
+            display: inline-block;
+            position: relative;
+            top: 1px;
+            width: 16px;
+            margin-right: 4px;
+        }
+
+        .footer-info {
+            float: right;
+            margin-top: 5px;
+            font-size: 0.75em;
+            color: #ccc;
+        }
+
+        .footer-info span {
+            padding: 0 5px;
+            color: black;
+        }
+
+        .footer-info span:last-child {
+            padding-right: 0;
+        }
+
+        .page-container {
+            display: none;
         }
     </style>
 </head>
 
 <body>
-
-
-    {{-- <footer>
-
-        <div style="text-align: center; color:#333; font-size: 11px !important;">
-            <p>{{ optional(getCompany())->name }}</p>
-            <p>
-                {{ optional(getCompany())->addresse }}
-                Tél : {{ optional(getCompany())->telephone }}
-                E-mail : {{ optional(getCompany())->email }}
-            </p>
-            <p>
-                -R.C:{{ optional(getCompany())->rc }}
-                -PATENTE:{{ optional(getCompany())->patente }}
-                -I.F:{{ optional(getCompany())->if }}
-                @if (isset(getCompany()->cnss))
-                    -CNSS:{{ optional(getCompany())->cnss }}
-                @endif
-                -ICE:{{ optional(getCompany())->ice }}
-            </p>
-        </div>
-    </footer> --}}
-    <div class="invoice-box">
-        <table>
-            <tr class="top">
-                <td colspan="6">
-                    <table>
-                        <tr>
-                            <td class="title" style="text-align: left;">
-                                <img src="{{ $companyLogo }}" style="height: 100px" />
-                            </td>
-
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <hr>
-            <tr class="heading">
-                <h3>Destinataire :</h3>
-                <strong>Nom complet : {{ $command->client_name }}</strong>
-                <strong>Téléphone : {{ $command->client_phone }}</strong>
-                <strong>Adresse : {{ $command->client_address }}</strong>
-                <strong>Ville : {{ $command->city->name }}</strong>
-                <strong>Date d'envoi : {{ $command->created_at->format('d-m-Y') }}</strong>
-            </tr>
-
-            <hr>
-
-            {{-- <div class="pricer">
-                <tr class="heading-price lefter">
-                    <td colspan="6">Montant BRUT : {{ number_format($invoice->formated_total_brut,2)}} DH</td>
-                </tr>
-                <tr class="heading-price lefter">
-                    @php
-                     $frais = $invoice->articles->sum('frais')
-                    @endphp
-                    <td colspan="6">Frais : {{ number_format($frais,2) }} DH</td>
-                </tr>
-                <tr class="heading-price lefter">
-                    @php 
-                        if($invoice->formated_total_brut == 0 || $frais > $invoice->formated_total_brut)
-                        {
-                            $net = 00;
-                        }
-                        else{
-                            $net = $invoice->formated_total_brut - $frais;
-                        }
-        
-                    @endphp
-                    <td colspan="6">Montant NET : {{ number_format($net,2) }} DH</td>
-                </tr>
-            </div> --}}
-
-        </table>
-
+    <div class="page-container">
+        Page
+        <span class="page"></span>
+        of
+        <span class="pages"></span>
     </div>
 
+    <div class="logo-container">
+        <img style="height: 100px" src="{{ $companyLogo }}">
+    </div>
 
-    <script type="text/php">
+    <table class="line-items-container has-bottom-border">
+        <thead>
+            <tr>
+                <th>Destinataire</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="payment-info">
+                    <div>
+                        Nom complet :<strong>{{ $command->client_name }}</strong>
+                    </div>
+                    <div>
+                        Téléphone :<strong>{{ $command->client_phone }}</strong>
+                    </div>
+                </td>
 
-        if (isset($pdf) && $PAGE_COUNT > 1) {
-            $text = "Page {PAGE_NUM} / {PAGE_COUNT}";
-            $size = 5;
-            $font = $fontMetrics->getFont("Verdana");
-            $width = $fontMetrics->get_text_width($text, $font, $size) / 2;
-            $x = ($pdf->get_width() - $width);
-            $y = $pdf->get_height() - 35;
-            $pdf->page_text($x, $y, $text, $font, $size);
-        }
+        </tbody>
+    </table>
+    <table class="line-items-container has-bottom-border">
+        <thead>
+            <tr>
+                <th>Command</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($command->items as $item)
+                <tr>
+                    <td class="payment-info">
+                        <div>
+                            Produit : <strong>{{$item->product}}</strong>
+                        </div>
+                        <div>
+                            Prix  : 
+                            <strong>
+                                {{ $item->prix_uni }} (DH) 
+                                    x
+                                {{ $item->quantity }}
+                            </strong>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
 
+        </tbody>
+    </table>
 
-</script>
+    <div class="footer">
+        <div class="footer-info">
+            <span>info@sameleon-express.ma</span> |
+            <span>555 444 6666</span> |
+            <span>sameleon-express.ma</span>
+        </div>
+        <div class="footer-thanks">
+            <img src="https://github.com/anvilco/html-pdf-invoice-template/raw/main/img/heart.png" alt="heart">
+            <span>Thank you!</span>
+        </div>
+    </div>
 </body>
 
 </html>
