3d6d5c5002affe05a467b2b0fef3697c7bcf6a25
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-10-19 18:04:30 +0100

    make a big update when Calculate Command frais + profit v-2

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -14,7 +14,7 @@ class CitySeeder extends Seeder
      */
     public function run()
     {
-        $cities = [
+        /*$cities = [
             ['name' => 'Casablanca', 'frais' => 14],
             ['name' => 'Mohammadia', 'frais' => 20],
             ['name' => 'Ain harrouda', 'frais' => 20],
@@ -26,6 +26,21 @@ class CitySeeder extends Seeder
             ['name' => 'Nouaceur', 'frais' => 20],
             ['name' => 'Sidi rahhal', 'frais' => 20],
             ['name' => 'Tit mellil', 'frais' => 20],
+            ['name' => 'Agadir', 'frais' => 25,'has_profit'=>true,'profit'=>15],
+        ];*/
+
+        $cities = [
+            ['name' => 'Casablanca', 'frais' => 14,'has_profit'=>true,'profit'=>0],
+            ['name' => 'Mohammadia', 'frais' => 20,'has_profit'=>true,'profit'=>10],
+            ['name' => 'Rabat', 'frais' => 20,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Fés', 'frais' => 30,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Marrakech', 'frais' => 30,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Tanger', 'frais' => 30,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Salé', 'frais' => 20,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Meknès', 'frais' => 30,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Oujda', 'frais' => 30,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Kénitra', 'frais' => 20,'has_profit'=>true,'profit'=>15],
+            ['name' => 'Tétouan', 'frais' => 30,'has_profit'=>true,'profit'=>15],
             ['name' => 'Agadir', 'frais' => 25,'has_profit'=>true,'profit'=>15],
         ];
 
