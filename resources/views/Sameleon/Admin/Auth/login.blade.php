b27718e8f403cbdbf9a16efdaf39886d619a35e7
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-10-26 19:49:43 +0100

    add Sec meta data

--- resources/views/Sameleon/Admin/Auth/login.blade.php
+++ resources/views/Sameleon/Admin/Auth/login.blade.php
@@ -8,8 +8,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex, nofollow" />
     <link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
-    <meta content="app_creator" name="Elmarzougui Abdelghafour" />
-    <meta content="app_version" name="v 1.1" />
+    <meta name="author" content="Elmarzougui Abdelghafour">
+    <meta name="app_version" content="1.0.0" />
+    <meta name="app_devlopper" content="Elmarzougui Abdelghafour" />
+    <meta name="app_devlopper_website" content="https://elmarzougui.com" />
+    <meta name="app_devlopper_facebook" content="https://www.facebook.com/devscript" />
+    <meta name="app_devlopper_linkedin" content="https://www.linkedin.com/in/devscript/" />
+    <meta name="app_devlopper_twitter"  content="https://twitter.com/devscriptt" />
+    <meta name="app_devlopper_github"   content="https://github.com/devscript-abdo" />
+
+    @include('layouts._parts.__sec_meta')
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
      @include('layouts._parts.__og_meta')
---
+++ resources/views/layouts/_parts/__sec_meta.blade.php
@@ -0,0 +1,8 @@
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+<meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+<meta http-equiv="X-WebKit-CSP" content="default-src 'self'; script-src 'self'">
+<meta content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff" http-equiv="Content-Type" />
+
+<meta name="referrer" content="none">
+
+<meta http-equiv="Feature-Policy" content="ch-viewport-width *">
