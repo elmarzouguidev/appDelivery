e9b022f58a3446d12feecf17dd6f83464d82cfe9
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-10-31 13:59:23 +0100

    remove devlopper info v2

--- resources/views/layouts/_parts/_footer.blade.php
+++ resources/views/layouts/_parts/_footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-12">
+            <div class="col-sm-6">
                 <script>document.write(new Date().getFullYear())</script> © SAMELEON EXPRESS
             </div>
-            {{--<div class="col-sm-6">
+            <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
                     Developed by <a title="Elmarzougui Abdelghafour" href="https://www.linkedin.com/in/devscript" target="_blank">Elmarzougui.com</a>
                 </div>
-            </div>--}}
+            </div>
         </div>
     </div>
 </footer>
