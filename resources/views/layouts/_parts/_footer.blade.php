b686bbb800a57a6a447c7447accf5629d89f9e93
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-11-07 16:08:48 +0100

    update

--- resources/views/layouts/_parts/_footer.blade.php
+++ resources/views/layouts/_parts/_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © SAMELEON EXPRESS
+                <script>document.write(new Date().getFullYear())</script> © <b>SAMELEON EXPRESS</b>
             </div>
             <!--<div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
