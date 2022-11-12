2d7718af45bd5acc7190eda31aec81ae04f8ae94
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-11-12 14:45:00 +0100

    fix same stuffs in Bon livraison

--- app/Repositories/BL/BLRepository.php
+++ app/Repositories/BL/BLRepository.php
@@ -39,6 +39,13 @@ class BLRepository extends AppRepository implements BLInterface
      */
     public function getBLs()
     {
+        if(isDelivery())
+        {
+            return $this->bl
+            ->where('delivery_id' , delivery()->id)
+            ->where('delivery_uuid' , delivery()->uuid)
+            ->with('articles')->with('city:id,name')->get();
+        }
         return $this->bl->with('articles')->with('city:id,name')->get();
     }
 
