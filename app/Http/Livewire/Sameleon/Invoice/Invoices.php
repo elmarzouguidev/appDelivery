d937f308b50677eaaeba65704950c6c079956472
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-11-14 10:59:56 +0100

    fix same stuff in Price bill -v3

--- app/Http/Livewire/Sameleon/Invoice/Invoices.php
+++ app/Http/Livewire/Sameleon/Invoice/Invoices.php
@@ -19,7 +19,6 @@ class Invoices extends Component
     /****Bill ****/
 
     public $price;
-    public $formatedPrice;
     public $date;
     public $mode;
     public $reference;
@@ -68,8 +67,6 @@ class Invoices extends Component
         ->loadSum('articles','profit');
         //$this->price = $invoice->articles_sum_price_total;
         $this->price =  number_format($invoice->articles_sum_price_total -($invoice->articles_sum_frais + $invoice->articles_sum_profit),2);
-        $this->formatedPrice = intval($this->price);
-        $this->price = intval($this->price);
         $this->dispatchBrowserEvent('add-bill');
     }
 
@@ -134,7 +131,7 @@ class Invoices extends Component
 
         return [
 
-            'price' => ['required', 'numeric'],
+            'price' => ['required', 'digits_between:2,5'],
             'date' => ['required', 'date'],
             'mode' => ['required', 'string'],
             'reference' => ['nullable', 'string'],
