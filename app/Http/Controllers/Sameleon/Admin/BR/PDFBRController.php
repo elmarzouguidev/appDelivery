2d89e68f38c770e3e91f89ea2583d4f730666fec
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-11-16 11:14:23 +0100

    add same stuff in BR PDF v-2

--- app/Http/Controllers/Sameleon/Admin/BR/PDFBRController.php
+++ app/Http/Controllers/Sameleon/Admin/BR/PDFBRController.php
@@ -13,7 +13,7 @@ class PDFBRController extends Controller
 
         $qrcode = base64_encode(QrCode::format('svg')->size(80)->errorCorrection('H')->generate('https://sameleon-express.ma/'));
 
-        $bon->load('articles', 'client:uuid,id,nom,prenom,','articles.command.items','client.city');
+        $bon->load('articles', 'client:uuid,id,nom,prenom','articles.command.items','client.city');
 
         $companyLogo = "data:image/jpg;base64," . base64_encode(file_get_contents(public_path('storage/' . getCompany()->logo)));
 
