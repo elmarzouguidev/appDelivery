154c7e81619a63b6fcd157561a3851fccc30dfd4
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-11-21 16:30:29 +0100

    add sorting by client name in product lists c-2

--- app/Repositories/Product/ProductRepository.php
+++ app/Repositories/Product/ProductRepository.php
@@ -51,7 +51,7 @@ class ProductRepository extends AppRepository implements ProductInterface
                 ->get()
                 ->sortBy(function ($query) {
                     return optional($query->client)->prenom;
-                })
+                }, SORT_REGULAR, false)
                 ->all();
         }
 
