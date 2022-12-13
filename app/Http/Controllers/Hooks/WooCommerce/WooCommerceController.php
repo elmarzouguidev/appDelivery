e95357c860ea6e3b81ea4ccc565dbdd87018cf03
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-12-13 18:07:29 +0100

    Working in Woocomerce Webhook V12

--- app/Http/Controllers/Hooks/WooCommerce/WooCommerceController.php
+++ app/Http/Controllers/Hooks/WooCommerce/WooCommerceController.php
@@ -76,7 +76,7 @@ class WooCommerceController extends Controller
             'user_id' => $this->client->id,
             'user_uuid' => $this->client->uuid,
             'frais' => $city ? $city->frais : 00,
-            'comment' => "Command added from Woocommerce for test : https://test.sameleon-express.ma/"
+            'comment' => "Cette commande a été ajouter pour TESTER l'ajoute via WooCommerce : https://test.sameleon-express.ma/"
         ]);
         collect($items['line_items'])->each(function ($item) use ($command) {
 
