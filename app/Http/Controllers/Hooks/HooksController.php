06f3f8f8a9b7b3d7854d1b19f04f197217cf763e
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-12-13 12:55:17 +0100

    update Hooks url ... v13

--- app/Http/Controllers/Hooks/HooksController.php
+++ app/Http/Controllers/Hooks/HooksController.php
@@ -17,7 +17,7 @@ class HooksController extends Controller
     public function woocommerce(Request $request)
     {
         logger('woocommerce had ben invoked hahah');
-        app(WooCommerceController::class, ['data' => $request]);
+        app(WooCommerceController::class, ['payload' => $request]);
     }
 
     public function elementor()
