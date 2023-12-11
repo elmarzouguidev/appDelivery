9c361ee4b3cc1a8e6f15f39b4593724ec3383ce2
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-12-11 17:41:05 +0100

    fix title in same login page

--- resources/views/Sameleon/Admin/SubDelivery/Auth/login.blade.php
+++ resources/views/Sameleon/Admin/SubDelivery/Auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Login Livreur | SAMELEON Express</title>
+    <title>Login Livreur | QuickRoute</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex, nofollow" />
     <link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
