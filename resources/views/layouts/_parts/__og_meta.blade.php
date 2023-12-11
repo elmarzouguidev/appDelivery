0621ac682ee94483cc11757a483dc2d1b6ab6e31
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-12-11 18:28:58 +0100

    fix error in PDF generattion and force update to laravel 10

--- resources/views/layouts/_parts/__og_meta.blade.php
+++ resources/views/layouts/_parts/__og_meta.blade.php
@@ -3,6 +3,6 @@
 <meta property="og:site_name" content="QuickRoute" />
 <meta property="og:type" content="website" />
 <meta property="og:locale" content="fr_FR" />
-<meta property="og:url" content="https://quickroute.haymacproduction.ma" />
+<meta property="og:url" content="https://quickroute.ma" />
 <meta property="og:image" itemprop="image" content="{{ asset('images/logo.png') }}" />
 <meta property="og:updated_time" content="1440432930" />
